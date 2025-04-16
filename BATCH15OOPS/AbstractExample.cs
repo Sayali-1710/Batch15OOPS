c9a3fce8fb532c77577be5099fe173b1dc1359a0
Sayali-1710 <sayalitodkar17102001@gmail.com>
2025-04-16 23:12:05 +0530

    added file by abstract

---
+++ BATCH15OOPS/AbstractExample.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPSAssign
+{
+
+    public abstract class shape()
+    {
+
+        //abstract method 
+        public abstract double calculateArea(double x, double y);
+        //non abstract method
+        public virtual void DisplayShape()
+        {
+            Console.WriteLine("This is a Shape");
+        }
+
+    }
+    //derived class
+    public class Circle : shape
+    {
+        public override double calculateArea(double pi, double radius)
+        {
+            return pi * radius;
+        }
+
+        public override void DisplayShape()
+        {
+            Console.WriteLine("This is a circle");
+        }
+
+
+    }
+
+    public class Rectangle : shape
+    {
+        public override double calculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public override void DisplayShape()
+        {
+            Console.WriteLine(" This is a Rectangle");
+        }
+
+
+    }
+
+
+    class AbstractExample
+    {
+        static void Main()
+        {
+            Circle circleArea = new Circle();
+            Rectangle rectangleArea = new Rectangle();
+
+            circleArea.DisplayShape();
+            Console.WriteLine("Area of Circle is :" + circleArea.calculateArea(9.38, 8));
+
+            rectangleArea.DisplayShape();
+            Console.WriteLine("Area of Rectangle is :" + rectangleArea.calculateArea(7.7, 60));
+
+        }
+
+
+
+    }
+}
