67dfd82bff11d88b1c1e4941969e7f0f534ae4b9
Sayali-1710 <sayalitodkar17102001@gmail.com>
2025-04-16 23:11:32 +0530

    added file by static

---
+++ BATCH15OOPS/DynamicPolymorphism.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPSAssign
+{
+
+    class Vehicle
+    {
+        public virtual void startengine()
+        {
+            Console.WriteLine("=====Vehicle Engine Started=====");
+        }
+    }
+
+    class car : Vehicle
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("car Engine Started");
+        }
+    }
+    class Truck : Vehicle
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("Truck Engine Started");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("Bike Engine started");
+        }
+    }
+    class Bus : Vehicle
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("Bus Engine started");
+        }
+
+    }
+    class Tractor : Vehicle
+    {
+
+        public override void startengine()
+        {
+            Console.WriteLine("Tractor Engine started");
+        }
+
+    }
+    class Aerialtramway : Vehicle
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("Aerialtramway Engine started");
+        }
+    }
+
+    class DynamicPolymorphism
+    {
+        static void Main()
+        {
+            Vehicle Vehicle1 = new Vehicle();
+            Vehicle vehicle2 = new car();
+            Vehicle vehicle3 = new Truck();
+            Vehicle vehicle4 = new Bike();
+            Vehicle vehicle5 = new Bus();
+            Vehicle vehicle6 = new Tractor();
+            Vehicle vehicle7 = new Aerialtramway();
+
+            Vehicle1.startengine();
+            vehicle2.startengine();
+            vehicle3.startengine();
+            vehicle4.startengine();
+            vehicle5.startengine();
+            vehicle6.startengine();
+            vehicle7.startengine();
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ BATCH15OOPS/StaticPolymorphism.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPSAssign
+{
+    class calculator
+    {
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        public double Add(double x, double y, int z)
+        {
+            return x + y + z;
+        }
+
+        public int subst(int x, int y)
+        {
+            return x - y;
+        }
+        public double subst(double x, double y)
+        {
+            return x - y;
+
+        }
+        public int multi(int x, int y)
+        {
+            return x * y;
+
+        }
+        public double multi(double x, double y)
+        {
+            return x * y;
+        }
+
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        public double div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+
+    class StaticPolymorphism
+    {
+        static void Main()
+        {
+            calculator c = new calculator();
+
+            int sum = c.Add(23, 56, 52);
+            double doublesum = c.Add(24.2, 12.6, 4);
+            int subst = c.subst(26, 29);
+            double doublesubst = c.subst(96.2, 16.9);
+            int multi = c.multi(101, 126);
+            double doublemulti = c.multi(46.23, 23.8);
+            int Div = c.Div(48, 24);
+            double doubleDiv = c.Div(186, 2);
+
+
+
+            Console.WriteLine($"Sum of integer value:  {sum}");
+            Console.WriteLine($"Sum of Decimal value:  {doublesum}");
+            Console.WriteLine($"Substract of integer value:  {subst}");
+            Console.WriteLine($"Substract of Decimal value:  {doublesubst}");
+            Console.WriteLine($"Multiplication of integer value:  {multi}");
+            Console.WriteLine($"Multiplication of Decimal value:  {doublemulti}");
+            Console.WriteLine($"Division of integer value:  {Div}");
+            Console.WriteLine($"Division of integer value:  {doubleDiv}");
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
