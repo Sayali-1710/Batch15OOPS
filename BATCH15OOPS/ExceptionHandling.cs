38ddad0805b26f2c74699ac8f2e0495da44767c2
Sayali-1710 <sayalitodkar17102001@gmail.com>
2025-05-05 23:04:48 +0530

    exception

---
+++ BATCH15OOPS/ExceptionHandling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPSAssign
+{
+    class ExceptionHandling
+    {
+        static void Main()
+        {
+            int number1 = 59;
+            int number2 = 0;
+
+            try
+            {
+             
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
